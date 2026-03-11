e98e8594174ef1718856b681a80e4be0e7986aea
Levi <levicamel123@gmail.com>
2026-03-11 17:41:48 -0300

    Testando por conta própria como funciona matriz em python.

--- 01 - Estrutura de dados/01 - Listas/03_matriz.py
+++ 01 - Estrutura de dados/01 - Listas/03_matriz.py
@@ -8,3 +8,13 @@ print(matriz[0])  # [1, "a", 2]
 print(matriz[0][0])  # 1
 print(matriz[0][-1])  # 2
 print(matriz[-1][-1])  # "c"
+
+matriz_01 = [
+    [1, 2, 3],
+    ["a", "b", "c"],
+    [5, "L", 8]
+]
+
+print(matriz_01[1]) # ["a", "b", "c"]
+print(matriz_01[1][-1]) # "c"
+print(matriz_01[-1][1]) # "L"
