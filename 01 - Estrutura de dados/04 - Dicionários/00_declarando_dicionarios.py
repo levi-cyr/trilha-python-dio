0df512a1b48004699c2090dabeecac82b95bf9aa
Levi <levicamel123@gmail.com>
2026-03-14 11:01:05 -0300

    testando mais possibilidades declarando dicionarios

--- 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
+++ 01 - Estrutura de dados/04 - Dicionários/00_declarando_dicionarios.py
@@ -6,3 +6,13 @@ print(pessoa)
 
 pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
 print(pessoa)
+
+#teste dos formatos de dicionario
+linguagem = {"lingua": ["Python", "C#", "Java"]}
+print(linguagem)
+
+linguagem = dict(lingua={"Python", "C#", "Java"})
+print(linguagem)
+
+linguagem["lingua"] = "Python", "C#", "Java"
+print(linguagem)
