2a360d6d85135d993c5a7b859dd90e8d34899c80
Levi <levicamel123@gmail.com>
2026-03-14 11:16:45 -0300

    testando como acessar dados que possuem uma chave com mais de 1 valor

--- 01 - Estrutura de dados/04 - Dicionários/01_acessando_dados.py
+++ 01 - Estrutura de dados/04 - Dicionários/01_acessando_dados.py
@@ -9,3 +9,11 @@ dados["idade"] = 18
 dados["telefone"] = "9988-1781"
 
 print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
+
+#posso armazenar mais de um dado na chave, como exemplo 2 nomes com 2 idades, 2 telefones, etc
+dados2 = {"nome": ["Levi", "Guilherme"], "idade": [26, 28]}
+
+print(dados2["nome"][0]) #acesso como uma array, especificando se quero a de ambos ou só de um em especifico
+print(dados2["nome"][1])
+
+print(dados2)
