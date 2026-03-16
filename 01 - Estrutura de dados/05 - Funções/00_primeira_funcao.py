e87891c59d67a7d874590b8ff19970e888126679
Levi <levicamel123@gmail.com>
2026-03-16 19:13:06 -0300

    testando funções

--- 01 - Estrutura de dados/05 - Funções/00_primeira_funcao.py
+++ 01 - Estrutura de dados/05 - Funções/00_primeira_funcao.py
@@ -9,8 +9,14 @@ def exibir_mensagem_2(nome):
 def exibir_mensagem_3(nome="Anônimo"):
     print(f"Seja bem vindo {nome}!")
 
+def somar():
+    num1 = int(input("Digite um número: "))
+    num2 = int(input("Digite outro número:" ))
+    soma = num1 + num2
+    print(f"A soma entre {num1} e {num2} é igual a {soma}")
 
-exibir_mensagem()
-exibir_mensagem_2(nome="Guilherme")
-exibir_mensagem_3()
-exibir_mensagem_3(nome="Chappie")
+#exibir_mensagem()
+#exibir_mensagem_2(nome="Guilherme")
+#exibir_mensagem_3()
+#exibir_mensagem_3(nome="Chappie")
+somar()
